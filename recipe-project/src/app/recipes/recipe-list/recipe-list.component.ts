25f277e1d0687999d436dc342973b73732d1d19e
Vit Herain <vit.herain@zonky.cz>
2017-06-11 12:35:40 +0200

    Section 3, Lecture 47

--- recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simplay a test', 'http://i.imgur.com/Jmk1Nva.jpg')
+  ];
 
   constructor() { }
 
