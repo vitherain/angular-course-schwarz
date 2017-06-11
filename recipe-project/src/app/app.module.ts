abeae14346d0a9d2b0adf466eeb53615a958e197
Vit Herain <vit.herain@zonky.cz>
2017-06-11 12:14:20 +0200

    Section 3, Lecture 43

--- recipe-project/src/app/app.module.ts
+++ recipe-project/src/app/app.module.ts
@@ -7,10 +7,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from "./header/header.component";
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
@@ -18,10 +18,10 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     HeaderComponent,
     ShoppingListComponent,
     RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailComponent,
-    RecipesItemComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
