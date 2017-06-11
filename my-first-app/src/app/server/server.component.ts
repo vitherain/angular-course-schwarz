f727ac093f4f6b5dba9052af74b741fc9a7d19cd
Vit Herain <vit.herain@zonky.cz>
2017-06-11 10:59:52 +0200

    Section 2, Lecture 36

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,7 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        background-color: green;
+        color: white;
+      }
+      .offline {
+        background-color: red;
+        color: white;
+      }
+    `
+  ]
 })
 export class ServerComponent {
   serverId: number = 10;
@@ -16,7 +28,7 @@ export class ServerComponent {
     return this.serverStatus;
   }
 
-  getColor() {
-    return this.serverStatus === 'online' ? 'green' : 'red';
-  }
+  //getColor() {
+  //  return this.serverStatus === 'online' ? 'green' : 'red';
+  //}
 }
