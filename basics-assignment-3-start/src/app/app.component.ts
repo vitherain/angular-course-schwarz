37f86be804d041f8efc510ab4a38574c1c610a6a
Vit Herain <vit.herain@zonky.cz>
2017-06-11 11:20:26 +0200

    Assignment 3 finished

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  displaySecretPassword: boolean = false;
+  clicksCounter: number = 0;
+  clicks: any[] = [];
+
+  toggleDisplaySecretPassword() {
+    this.displaySecretPassword = !this.displaySecretPassword;
+    this.clicks.push(
+      {
+        id: ++this.clicksCounter,
+        timestamp: new Date()
+      }
+    );
+  }
 }
