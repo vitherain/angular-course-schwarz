2877b1092a185278ab9e1b5ed8fc1580bfef79ab
Vit Herain <vit.herain@zonky.cz>
2017-06-11 10:01:28 +0200

    Section 2, Lecture 29

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = '';
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
