a1a5d42fc194b19ac745a3aece4d1c16641b61be
Vit Herain <vit.herain@zonky.cz>
2017-07-02 18:23:26 +0200

    Section 10, Lecture 107

--- recipe-project/src/app/app.module.ts
+++ recipe-project/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { DropdownDirective } from "./shared/dropdown.directive";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { DropdownDirective } from "./shared/dropdown.directive";
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- recipe-project/src/app/shopping-list/shopping-list.service.ts
+++ recipe-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,5 +1,19 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
   constructor() { }
 
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
