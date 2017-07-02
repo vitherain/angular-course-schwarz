b04b7141848336c58bfb6010e4895fe2b06d8213
Vit Herain <vit.herain@zonky.cz>
2017-07-02 14:38:23 +0200

    Section 10, Lecture 105

--- recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,14 +11,12 @@ export class RecipeListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://i.imgur.com/Jmk1Nva.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'http://i.imgur.com/Jmk1Nva.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- recipe-project/src/app/recipes/recipe.service.ts
+++ recipe-project/src/app/recipes/recipe.service.ts
@@ -1,5 +1,15 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://i.imgur.com/Jmk1Nva.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'http://i.imgur.com/Jmk1Nva.jpg')
+  ];
 
   constructor() { }
 
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
 }
