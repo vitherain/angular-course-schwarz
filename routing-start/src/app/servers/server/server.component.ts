2c3bb3afb175d4467754cca78c32fa55369e9a3b
Vit Herain <vit.herain@zonky.cz>
2017-07-06 13:51:52 +0200

    Section 11, Lecture 139

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -16,7 +16,12 @@ export class ServerComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
+      }
+    );
+    /*const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     //this.server.name = this.route.snapshot.params['name'];
     //this.server.status = this.route.snapshot.params['status'];
@@ -24,7 +29,7 @@ export class ServerComponent implements OnInit {
       (params: Params) => {
         this.server = this.serversService.getServer(+params['id']);
       }
-    );
+    );*/
   }
 
   onEdit() {
