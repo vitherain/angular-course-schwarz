4c1ae32ee689b337f1df91c830f931889f705f60
Vit Herain <vit.herain@zonky.cz>
2017-07-15 11:31:53 +0200

    Assignment 6 finished

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('frm') frm: NgForm;
+  @ViewChild('email') email: FormControl;
+  defaultSubscription: string = 'advanced';
+
+  onSubmit() {
+    console.log(this.email);
+    console.log(this.frm);
+  }
 }
