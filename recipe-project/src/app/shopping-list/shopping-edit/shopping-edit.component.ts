6cc53d88790d6f9d3be1f6ffb070df1227deffa0
Vit Herain <vit.herain@zonky.cz>
2017-08-06 09:46:25 +0200

    Section 16, Lecture 203

--- recipe-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Ingredient } from "../../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list.service";
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.slService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+        }
+      );
   }
 
   onAddItem(form: NgForm) {
@@ -21,4 +33,7 @@ export class ShoppingEditComponent implements OnInit {
     this.slService.addIngredient(newIngredient);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
