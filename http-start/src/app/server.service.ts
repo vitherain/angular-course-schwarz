1ca9475a2f67add29309a24bf2545b715a979b2b
Vit Herain <vit.herain@zonky.cz>
2017-08-13 14:03:02 +0200

    Section 18, Lecture 234

--- http-start/src/app/server.service.ts
+++ http-start/src/app/server.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class ServerService {
   constructor(private http: Http) {}
 
   storeServers(servers: any[]) {
-    return this.http.post('https://udemy-ng-http-49f40.firebaseio.com/data.json', servers);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.post(
+      'https://udemy-ng-http-49f40.firebaseio.com/data.json',
+      servers,
+      { headers: headers }
+    );
   }
 }
