d35e2cba6b2ffb4fc25ee99950aa2f8418376cd6
Vit Herain <vit.herain@zonky.cz>
2017-08-20 10:03:11 +0200

    Section 19, Lecture 245

--- recipe-project/src/app/shared/data-storage.service.ts
+++ recipe-project/src/app/shared/data-storage.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+import 'rxjs/Rx';
+
 import { RecipeService } from "../recipes/recipe.service";
-import {Recipe} from "../recipes/recipe.model";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable()
 export class DataStorageService {
@@ -15,9 +17,19 @@ export class DataStorageService {
 
   getRecipes() {
     return this.http.get('https://ng-recipe-book-ce900.firebaseio.com/recipes.json')
-      .subscribe(
+      .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
           this.recipeService.setRecipes(recipes);
         }
       );
