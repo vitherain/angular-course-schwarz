db4a77b9a45acf33bb4dc0611ca26f5a5ecbdb9a
Vit Herain <vit.herain@zonky.cz>
2017-08-20 09:52:03 +0200

    Section 19, Lecture 243

--- recipe-project/src/app/header/header.component.ts
+++ recipe-project/src/app/header/header.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  constructor(private dsService: DataStorageService) {}
+
+  onSaveData() {
+    this.dsService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
 }
