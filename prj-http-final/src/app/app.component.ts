1ebb081299925f5cc22da004abb5d5ccea9730a3
Vit Herain <vit.herain@zonky.cz>
2017-09-03 10:27:19 +0200

    Section 20, Lecture 250

--- prj-http-final/src/app/app.component.ts
+++ prj-http-final/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp(
+      {
+        apiKey: "AIzaSyB1TfFJt1Sa4JhyGvw5eJv2S9g9lL4jW-U",
+        authDomain: "ng-recipe-book-ce900.firebaseapp.com"
+      }
+    );
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
