cc636065011898c276abd08ad7a7b57982113a49
Vit Herain <vit.herain@zonky.cz>
2017-09-03 11:24:03 +0200

    Section 20, Lecture 257

--- prj-http-final/src/app/app.module.ts
+++ prj-http-final/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthService } from "./auth/auth.service";
 import { DataStorageService } from './shared/data-storage.service';
+import { AuthGuard } from "./auth/auth-guard.service";
 
 @NgModule({
   declarations: [
@@ -45,7 +46,7 @@ import { DataStorageService } from './shared/data-storage.service';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- prj-http-final/src/app/auth/auth.service.ts
+++ prj-http-final/src/app/auth/auth.service.ts
@@ -1,8 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 import * as firebase from 'firebase';
 
+@Injectable()
 export class AuthService {
 
   token: string;
+
+  constructor(private router: Router) {}
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -15,6 +21,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getToken()
             .then(
               (token: string) => this.token = token
