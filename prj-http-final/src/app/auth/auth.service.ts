dba4b721602eac6c80dda2f68baf2429e915ee42
Vit Herain <vit.herain@zonky.cz>
2017-09-03 10:56:54 +0200

    Section 20, Lecture 254

--- prj-http-final/src/app/auth/auth.service.ts
+++ prj-http-final/src/app/auth/auth.service.ts
@@ -2,6 +2,8 @@ import * as firebase from 'firebase';
 
 export class AuthService {
 
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
@@ -12,10 +14,25 @@ export class AuthService {
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
-        response => console.log(response)
+        response => {
+          firebase.auth().currentUser.getToken()
+            .then(
+              (token: string) => this.token = token
+            );
+          console.log(response)
+        }
       )
       .catch(
         error => console.log(error)
       );
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getToken()
+      .then(
+        (token: string) => this.token = token
+      );
+
+    return this.token;
   }
 }
