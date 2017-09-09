504a9cbfc064a0aa2a84dcf9dde34d8bae125353
Vít Herain <vit.herain@zonky.cz>
2017-09-09 10:35:01 +0200

    Section 21, Lecture 278

--- optimizations-feature-shared-module/src/app/recipes/recipes-routing.module.ts
+++ optimizations-feature-shared-module/src/app/recipes/recipes-routing.module.ts
@@ -20,6 +20,7 @@ const recipesRoutes: Routes = [
   imports: [
     RouterModule.forChild(recipesRoutes)
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class RecipesRoutingModule {}
