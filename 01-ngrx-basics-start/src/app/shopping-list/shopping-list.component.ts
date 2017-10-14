4fd8341477a3bfb0da88cd9d7a6d570bb28986fc
Vít Herain <vit.herain@zonky.cz>
2017-10-14 11:19:16 +0200

    Section 24, Lecture 309

--- 01-ngrx-basics-start/src/app/shopping-list/shopping-list.component.ts
+++ 01-ngrx-basics-start/src/app/shopping-list/shopping-list.component.ts
@@ -6,6 +6,8 @@ import { ShoppingListService } from './shopping-list.service';
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs/Observable";
 
+import * as fromShoppingList from './store/shopping-list.reducers';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -15,7 +17,7 @@ export class ShoppingListComponent implements OnInit {
   shoppingListState: Observable<{ingredients: Ingredient[]}>;
   private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService, private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
+  constructor(private slService: ShoppingListService, private store: Store<fromShoppingList.AppState>) { }
 
   ngOnInit() {
     this.shoppingListState = this.store.select('shoppingList');
