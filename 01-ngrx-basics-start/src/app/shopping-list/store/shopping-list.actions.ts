ef9cd1ffb98942e89e94b86cb0e3e71f666b14d4
Vít Herain <vit.herain@zonky.cz>
2017-10-14 10:23:59 +0200

    Section 24, Lecture 306

--- 01-ngrx-basics-start/src/app/shopping-list/store/shopping-list.actions.ts
+++ 01-ngrx-basics-start/src/app/shopping-list/store/shopping-list.actions.ts
@@ -7,7 +7,8 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export class AddIngredient implements Action {
 
   readonly type: string = ADD_INGREDIENT;
-  payload: Ingredient;
+
+  constructor(public payload: Ingredient) {}
 
 }
 
