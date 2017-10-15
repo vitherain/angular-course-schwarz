251737f95a1eb1c4325b656daa8f9a43498e46c8
Vít Herain <vit.herain@zonky.cz>
2017-10-15 10:31:16 +0200

    Section 24, Lecture 316

--- 01-ngrx-basics-start/src/app/app.module.ts
+++ 01-ngrx-basics-start/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthModule } from './auth/auth.module'
 import { CoreModule } from './core/core.module';
 import { StoreModule } from "@ngrx/store";
 import { shoppingListReducer } from "app/shopping-list/store/shopping-list.reducers";
+import { reducers } from './store/app.reducers';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,7 @@ import { shoppingListReducer } from "app/shopping-list/store/shopping-list.reduc
     ShoppingListModule,
     AuthModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot(reducers)
   ],
   bootstrap: [AppComponent]
 })
