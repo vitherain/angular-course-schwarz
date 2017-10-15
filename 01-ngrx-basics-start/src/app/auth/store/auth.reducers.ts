d92e4162eed8132ccc2d15f271062e794c7315c5
Vít Herain <vit.herain@zonky.cz>
2017-10-15 10:27:49 +0200

    Section 24, Lecture 315

--- 01-ngrx-basics-start/src/app/auth/store/auth.reducers.ts
+++ 01-ngrx-basics-start/src/app/auth/store/auth.reducers.ts
@@ -1,3 +1,5 @@
+import * as AuthActions from './auth.actions';
+
 export interface State {
     token: string,
     authenticated: boolean
@@ -8,8 +10,23 @@ export interface State {
     authenticated: false
   };
 
-export function authReducer(state = initialState, action) {
-    
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+    switch (action.type) {
+        case (AuthActions.SIGNUP):
+        case (AuthActions.SIGNIN):
+            return {
+                ...state,
+                authenticated: true
+            };
+        case (AuthActions.LOGOUT):
+            return {
+                ...state,
+                token: null,
+                authenticated: false
+            };
+        default:
+            return state;
+    }
 
     return state;
 }
