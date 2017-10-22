7eac209b588e0b4194771333b1d44e7200e988b7
Vít Herain <vit.herain@zonky.cz>
2017-10-22 12:10:20 +0200

    Section 24, Lecture 328

--- 01-ngrx-basics-start/src/app/core/header/header.component.ts
+++ 01-ngrx-basics-start/src/app/core/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 // import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 import { DataStorageService } from '../../shared/data-storage.service';
-import { AuthService } from '../../auth/auth.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../../store/app.reducers';
 import * as fromAuth from '../../auth/store/auth.reducers';
 import { Observable } from 'rxjs/Observable';
+
+import * as AuthActions from '../../auth/store/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -19,7 +20,6 @@ export class HeaderComponent implements OnInit {
     this.authState = this.store.select('auth');
   }
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService,
               private store: Store<fromApp.AppState>) {
   }
 
@@ -37,6 +37,6 @@ export class HeaderComponent implements OnInit {
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());    
   }
 }
