d4276fbe9a04790e110ef8ad6acb04a318c45b9e
Vít Herain <vit.herain@zonky.cz>
2017-10-22 11:42:04 +0200

    Section 24, Lecture 324

--- 01-ngrx-basics-start/src/app/auth/store/auth.effects.ts
+++ 01-ngrx-basics-start/src/app/auth/store/auth.effects.ts
@@ -1,11 +1,14 @@
 import { Effect, Actions } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 
+import * as AuthActions from './auth.actions';
+
 @Injectable()
 export class AuthEffects {
     
     @Effect()
-    authSignup;
+    authSignup = this.actions$
+        .ofType(AuthActions.TRY_SIGNUP);
 
     constructor(private actions$: Actions) {
     }
