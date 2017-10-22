c16faba3aa105ad2b197ba15b6a68b18e5fe1452
Vít Herain <vit.herain@zonky.cz>
2017-10-22 11:56:11 +0200

    Section 24, Lecture 325

--- 01-ngrx-basics-start/src/app/auth/store/auth.effects.ts
+++ 01-ngrx-basics-start/src/app/auth/store/auth.effects.ts
@@ -1,6 +1,12 @@
 import { Effect, Actions } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/mergeMap';
+import { fromPromise }  from 'rxjs/observable/fromPromise';
+import * as firebase from 'firebase';
+
 import * as AuthActions from './auth.actions';
 
 @Injectable()
@@ -8,7 +14,27 @@ export class AuthEffects {
     
     @Effect()
     authSignup = this.actions$
-        .ofType(AuthActions.TRY_SIGNUP);
+        .ofType(AuthActions.TRY_SIGNUP)
+        .map((action: AuthActions.TrySignup) => {
+            return action.payload;
+        })
+        .switchMap((authData: {username: string, password: string}) => {
+            return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+        })
+        .switchMap(() => {
+            return fromPromise(firebase.auth().currentUser.getIdToken());
+        })
+        .mergeMap((token: string) => {
+            return [
+                {
+                    type: AuthActions.SIGNUP
+                },
+                {
+                    type: AuthActions.SET_TOKEN,
+                    payload: token
+                }
+            ];
+        });
 
     constructor(private actions$: Actions) {
     }
