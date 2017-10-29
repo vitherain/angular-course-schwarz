11adf0535f879065b2cdece59e24f3903521ab56
Vít Herain <vit.herain@zonky.cz>
2017-10-29 10:40:22 +0100

    Section 24, Lecture 340

---
+++ 01-ngrx-basics-start/src/app/recipes/store/recipe.effects.ts
@@ -0,0 +1,38 @@
+import { Effect, Actions } from "@ngrx/effects";
+import { Injectable } from "@angular/core";
+import 'rxjs/add/operator/switchMap';
+
+import * as RecipeActions from '../store/recipe.actions';
+import { HttpClient } from "@angular/common/http";
+import { Recipe } from "app/recipes/recipe.model";
+
+@Injectable()
+export class RecipeEffects {
+
+    @Effect()
+    recipeFetch = this.actions$
+        .ofType(RecipeActions.FETCH_RECIPES)
+        .switchMap((action: RecipeActions.FetchRecipes) => {
+            this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', {
+                observe: 'body',
+                responseType: 'json'
+              })
+                
+        }).map(
+            (recipes) => {
+              console.log(recipes);
+              for (let recipe of recipes) {
+                if (!recipe['ingredients']) {
+                  recipe['ingredients'] = [];
+                }
+              }
+              return {
+                  type: RecipeActions.SET_RECIPES,
+                  payload: recipes
+              };
+            }
+          );
+
+    constructor(private actions$: Actions, 
+                private httpClient: HttpClient) {}
+}
