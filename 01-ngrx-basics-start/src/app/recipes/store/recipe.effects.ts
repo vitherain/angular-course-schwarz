b4df3163d03accca087275babfd43d04d390cae6
Vít Herain <vit.herain@zonky.cz>
2017-10-29 10:51:30 +0100

    Section 24, Lecture 341

--- 01-ngrx-basics-start/src/app/recipes/store/recipe.effects.ts
+++ 01-ngrx-basics-start/src/app/recipes/store/recipe.effects.ts
@@ -1,10 +1,14 @@
 import { Effect, Actions } from "@ngrx/effects";
 import { Injectable } from "@angular/core";
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/withLatestFrom';
 
 import * as RecipeActions from '../store/recipe.actions';
 import { HttpClient } from "@angular/common/http";
 import { Recipe } from "app/recipes/recipe.model";
+import { Store } from "@ngrx/store";
+import * as fromRecipe from '../store/recipe.reducers';
+import { HttpRequest } from "@angular/common/http";
 
 @Injectable()
 export class RecipeEffects {
@@ -33,6 +37,16 @@ export class RecipeEffects {
             }
           );
 
+    @Effect({dispatch: false})
+    recipeStore = this.actions$
+          .ofType(RecipeActions.STORE_RECIPES)
+          .withLatestFrom(this.store.select('recipes'))
+          .switchMap(([action, state]) => {
+            const req = new HttpRequest('PUT', 'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', state.recipes, {reportProgress: true});
+            return this.httpClient.request(req);
+          });
+
     constructor(private actions$: Actions, 
-                private httpClient: HttpClient) {}
+                private httpClient: HttpClient, 
+                private store: Store<fromRecipe.FeatueState>) {}
 }
