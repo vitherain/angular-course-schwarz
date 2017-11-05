792237d085e6320d91824bcb0df30f94117a32eb
Vít Herain <vit.herain@zonky.cz>
2017-11-05 13:23:16 +0100

    Section 26, Lecture 359

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { 
   Component, 
+} from '@angular/core';
+
+import {
   trigger,
   state,
-  style
-} from '@angular/core';
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -17,14 +22,40 @@ import {
       state('highlighted', style({
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
-      }))
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      //transition('highlighted => normal', animate(800)).
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
+
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 }
