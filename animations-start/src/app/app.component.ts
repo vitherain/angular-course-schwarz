166cef62e1f6632a87ee84a26b22fab396c1c458
Vít Herain <vit.herain@zonky.cz>
2017-11-05 13:44:12 +0100

    Section 26, Lecture 361

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -50,12 +50,32 @@ import {
         })),
         animate(500)
       ])
-    ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
   ]
 })
 export class AppComponent {
   state = 'normal';
   wildState = 'normal';
+  list = ['Milk', 'Beer'];
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
@@ -64,5 +84,13 @@ export class AppComponent {
 
   onShrink() {
     this.wildState = 'shrunken';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
   }
 }
